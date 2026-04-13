2e2461a0fc730e8d7e68926ad6d361c5ec51a8bb
Chaitanya Rathpd <rathodchaitanya39@gmail.com>
2026-04-13 10:43:43 +0530

    deploying

--- E_Auction/asgi.py
+++ E_Auction/asgi.py
@@ -1,25 +1,10 @@
 """
 ASGI config for E_Auction project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import Dashboard.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'E_Auction.settings')
 
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            Dashboard.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_asgi_application()
